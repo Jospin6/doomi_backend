9c8e43818e80c33379ee069680e16e2c373be1ca
Jospin <jospinndagano1@gmail.com>
2025-03-24 14:16:25 +0100

    create subscription

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { LocationModule } from './location/location.module';
 import { BusinessProfileModule } from './business-profile/business-profile.module';
+import { SubscriptionModule } from './subscription/subscription.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule, LocationModule, BusinessProfileModule],
+  imports: [AuthModule, PrismaModule, UserModule, LocationModule, BusinessProfileModule, SubscriptionModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
