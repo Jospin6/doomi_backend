0915325039594ec9d24c5131689b5b58a5e8fab8
Jospin <jospinndagano1@gmail.com>
2025-03-24 14:13:45 +0100

    Create location ressource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule],
+  imports: [AuthModule, PrismaModule, UserModule, LocationModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
